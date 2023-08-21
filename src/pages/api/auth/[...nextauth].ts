5b2e79af89ff37e64408733cfd7c83c773046d7a
Tyler Benning <tbenning@github.com>
2023-08-21 10:50:54 -0700

    update the env variable names

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import GithubProvider from "next-auth/providers/github";
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_APP_CLIENT_ID,
+      clientSecret: process.env.GITHUB_APP_SECRET,
       authorization: {
         params: { scope: "read:user user:email notifications public_repo" },
       },
